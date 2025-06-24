c91314da35ee4488c9dfee06a44d575e2500b481
simo <49877847+saimouu@users.noreply.github.com>
2025-06-24 14:58:33 +0300

    Remove unused api endpoints and dash page for only labeled data

--- api/routes.py
+++ api/routes.py
@@ -13,7 +13,7 @@ from api.models.anomaly_detection_params import AnomalyDetectionParams
 
 from services.loader import Loader
 from services.enhancer import Enhancer
-from services.log_analysis_pipeline import LogAnalysisPipeline, ManualTrainTestPipeline
+from services.log_analysis_pipeline import ManualTrainTestPipeline
 from utils.run_level_analysis import (
     unique_terms_count_by_run,
     files_and_lines_count,
@@ -26,74 +26,6 @@ from utils.umap_analysis import create_umap_embeddings, create_umap_df
 from utils.log_distance import measure_distances
 
 analyze_bp = Blueprint("main", __name__)
-
-
-# TODO: add masking option
-@analyze_bp.route("/analyze", methods=["POST"])
-def analyze():
-    params = request.get_json()
-
-    dir_path = params.get("dir_path")
-    models = params.get("models", ["lr"])
-    test_frac = float(params.get("test_frac", 0.9))
-    item_list_col = params.get("item_list_col", "e_words")
-    log_format = params.get("log_format", "lo2")
-    sequence_enhancement = params.get("seq", False)
-
-    if not dir_path or not os.path.exists(dir_path):
-        return (
-            jsonify({"error": "No directory specified or directory does not exist"}),
-            400,
-        )
-
-    results = None
-    pipeline = None
-    buffer = None
-
-    try:
-        pipeline = LogAnalysisPipeline(
-            directory_path=dir_path,
-            model_names=models,
-            item_list_col=item_list_col,
-            test_frac=test_frac,
-            log_format=log_format,
-            sequence_enhancement=sequence_enhancement,
-        )
-        pipeline.load()
-        pipeline.enhance()
-        pipeline.analyze()
-
-        if log_format == "raw":
-            results = pipeline.results
-        elif not sequence_enhancement:
-            results = pipeline.results.sort(["run", "m_timestamp"])
-        else:
-            results = pipeline.results.sort(["seq_id"])
-
-        buffer = io.BytesIO()
-        results.write_parquet(buffer, compression="zstd")  # zstd lz4
-        buffer.seek(0)
-
-        return Response(buffer.getvalue(), mimetype="application/octet-stream")
-
-    except Exception as e:
-        trace = traceback.format_exc()
-        logging.error(trace)
-
-        return jsonify({"error": str(e)}), 500
-
-    finally:
-        if buffer:
-            buffer.close()
-
-        # Might help with some memory issues, if python
-        # for whatever reason doesn't drop them automatically
-        if results is not None:
-            del results
-        if pipeline is not None:
-            del pipeline
-
-        gc.collect()
 
 
 @analyze_bp.route("/manual-test-train", methods=["POST"])
@@ -171,6 +103,8 @@ def manual_test_train():
         if buffer:
             buffer.close()
 
+        # Might help with some memory issues, if python
+        # for whatever reason doesn't drop them automatically
         if results is not None:
             del results
         if pipeline is not None:
@@ -353,48 +287,6 @@ def run_distance():
 
         buffer = io.BytesIO()
         result.write_parquet(buffer, compression="zstd")
-        buffer.seek(0)
-
-        return Response(buffer.getvalue(), mimetype="application/octet-stream")
-
-    except Exception as e:
-        trace = traceback.format_exc()
-        logging.error(trace)
-
-        return jsonify({"error": str(e)}), 500
-    finally:
-        if buffer:
-            buffer.close()
-
-
-@analyze_bp.route("/run-line-counts", methods=["POST"])
-def run_line_lens():
-    params = request.get_json()
-
-    dir_path = params.get("dir_path")
-    log_format = params.get("log_format", "lo2")
-
-    if not dir_path or not os.path.exists(dir_path):
-        return (
-            jsonify({"error": "No directory specified or directory does not exist"}),
-            400,
-        )
-
-    buffer = None
-    try:
-        loader = Loader(dir_path, log_format)
-        loader.load()
-        df = loader.df
-
-        line_counts = df.group_by("run").agg(
-            [
-                pl.len().alias("line_count"),
-                pl.col("seq_id").n_unique().alias("file_count"),
-            ]
-        )
-
-        buffer = io.BytesIO()
-        line_counts.write_parquet(buffer, compression="zstd")
         buffer.seek(0)
 
         return Response(buffer.getvalue(), mimetype="application/octet-stream")
