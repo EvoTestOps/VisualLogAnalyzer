41d6246cc0fa457d98fb24aea67e65d77b14507a
simo <49877847+saimouu@users.noreply.github.com>
2025-07-01 14:46:43 +0300

    Encapsulate instance creation to a function. Run in debug mode for the time being

--- main.py
+++ main.py
@@ -11,28 +11,33 @@ from dash_app.components.layouts import create_root_layout
 
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 
-server = Flask(__name__)
-server.register_blueprint(analyze_bp, url_prefix="/api")
-server.register_blueprint(dash_redirects_bp)
 
-dash_app = Dash(
-    __name__,
-    server=server,
-    url_base_pathname="/dash/",
-    use_pages=True,
-    pages_folder="dash_app/pages",
-    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME, dbc_css],
-)
+def create_app():
+    server = Flask(__name__)
+    server.register_blueprint(analyze_bp, url_prefix="/api")
+    server.register_blueprint(dash_redirects_bp)
 
-dash_app.layout = html.Div(
-    [
-        create_root_layout(),
-        dash.page_container,
-    ]
-)
+    dash_app = Dash(
+        __name__,
+        server=server,
+        url_base_pathname="/dash/",
+        use_pages=True,
+        pages_folder="dash_app/pages",
+        external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.FONT_AWESOME, dbc_css],
+    )
 
-color_switch_callback()
+    dash_app.layout = html.Div(
+        [
+            create_root_layout(),
+            dash.page_container,
+        ]
+    )
+
+    color_switch_callback()
+
+    return server
 
 
 if __name__ == "__main__":
-    server.run(debug=True)
+    app = create_app()
+    app.run(debug=True)
