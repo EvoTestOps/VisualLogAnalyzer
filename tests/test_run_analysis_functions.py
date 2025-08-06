0baa6de9d58c60a203e0eaae3242a9722f248fb2
simo <49877847+saimouu@users.noreply.github.com>
2025-08-06 15:45:03 +0300

    Add tests for anomaly detection run function

--- tests/test_run_analysis_functions.py
+++ tests/test_run_analysis_functions.py
@@ -1,4 +1,5 @@
 import pytest
+import polars as pl
 from unittest.mock import MagicMock, patch
 
 import server.analysis.analysis_runners as ar
@@ -52,7 +53,7 @@ def test_run_umap_analysis_directory_level_valid_inputs(mock_add_result):
 
     result = ar.run_umap_analysis(
         project_id=7,
-        directory_path=HIDDEN_GROUP,
+        directory_path=LABELED,
         item_list_col="e_words",
         file_level=False,
         vectorizer="count",
@@ -255,3 +256,138 @@ def test_run_log_distance_analysis_file_level_invalid_comparison(
             vectorizer="count",
             mask_type="myllari",
         )
+
+
+@pytest.mark.parametrize(
+    "file_level,directory_level,match_filenames,expected_type",
+    [
+        (True, False, True, "ano-file-level"),
+        (False, True, True, "ano-directory-level"),
+        (False, False, True, "ano-line-level"),
+        (True, False, False, "ano-file-level"),
+        (False, True, False, "ano-directory-level"),
+        (False, False, False, "ano-line-level"),
+    ],
+    ids=[
+        "file+match",
+        "dir+match",
+        "line+match",
+        "file+no-match",
+        "dir+no-match",
+        "line+no-match",
+    ],
+)
+@patch("server.analysis.analysis_runners.Settings")
+@patch("server.analysis.utils.analysis_helpers._add_result")
+def test_run_log_anomaly_detection_analysis_valid_inputs(
+    mock_add_result,
+    mock_settings_class,
+    file_level,
+    directory_level,
+    match_filenames,
+    expected_type,
+):
+
+    mock_settings = MagicMock()
+    mock_settings.match_filenames = match_filenames
+    mock_settings_class.query.filter_by.return_value.first_or_404.return_value = (
+        mock_settings
+    )
+
+    mock_add_result.return_value = 42
+
+    result = ar.run_anomaly_detection_analysis(
+        project_id=7,
+        train_data_path=ONLY_CORRECT_CASES,
+        test_data_path=HIDDEN_GROUP,
+        models=["kmeans", "oovd"],
+        item_list_col="e_words",
+        runs_to_include=None,
+        files_to_include=None,
+        file_level=file_level,
+        directory_level=directory_level,
+        mask_type="myllari",
+        vectorizer="count",
+    )
+
+    assert result["id"] == 42
+    assert result["type"] == expected_type
+
+
+@pytest.mark.parametrize(
+    "file_level,directory_level,match_filenames,expected_calls",
+    [
+        (True, False, True, ["analyze_file_group_by_filenames"]),
+        (False, False, True, ["analyze_line_group_by_filenames"]),
+        (False, True, True, ["aggregate_to_run_level", "analyze"]),
+        (True, False, False, ["aggregate_to_file_level", "analyze"]),
+        (False, True, False, ["aggregate_to_run_level", "analyze"]),
+        (False, False, False, ["analyze"]),
+    ],
+    ids=[
+        "file+match",
+        "line+match",
+        "dir+match",
+        "file+no-match",
+        "dir+no-match",
+        "line+no-match",
+    ],
+)
+@patch("server.analysis.analysis_runners.Settings")
+@patch("server.analysis.analysis_runners.store_and_format_result")
+@patch("server.analysis.analysis_runners.ManualTrainTestPipeline")
+def test_run_anomaly_detection_analysis_branching(
+    mock_pipeline_class,
+    mock_store_result,
+    mock_settings_class,
+    file_level,
+    directory_level,
+    match_filenames,
+    expected_calls,
+):
+
+    mock_settings = MagicMock()
+    mock_settings.match_filenames = match_filenames
+    mock_settings_class.query.filter_by.return_value.first_or_404.return_value = (
+        mock_settings
+    )
+
+    mock_pipeline = MagicMock()
+    # mock_pipeline.results = MagicMock()
+    mock_pipeline.results = pl.DataFrame(
+        {"e_words": [["a", "b"]], "kmeans_pred_ano_proba": [0.2]}
+    )
+    mock_pipeline_class.return_value = mock_pipeline
+    mock_store_result.return_value = {
+        "id": 1,
+        "type": f"ano-{'file' if file_level else 'directory' if directory_level else 'line'}-level",
+    }
+
+    _ = ar.run_anomaly_detection_analysis(
+        project_id=456,
+        train_data_path="some/train/path",
+        test_data_path="some/test/path",
+        models=["kmeans"],
+        item_list_col="e_words",
+        runs_to_include=None,
+        files_to_include=None,
+        file_level=file_level,
+        directory_level=directory_level,
+        mask_type="myllari",
+        vectorizer="count",
+    )
+
+    methods = [
+        "analyze_file_group_by_filenames",
+        "analyze_line_group_by_filenames",
+        "aggregate_to_run_level",
+        "aggregate_to_file_level",
+        "analyze",
+    ]
+
+    for method in methods:
+        method_mock = getattr(mock_pipeline, method)
+        if method in expected_calls:
+            method_mock.assert_called_once()
+        else:
+            method_mock.assert_not_called()
