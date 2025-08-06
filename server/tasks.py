c9bc602f0bae83a550ba4e79d08e0036e582d2d1
simo <49877847+saimouu@users.noreply.github.com>
2025-08-06 11:45:46 +0300

    Refactor analysis task logic to run functions

--- server/tasks.py
+++ server/tasks.py
@@ -5,33 +5,12 @@ from datetime import datetime, timezone
 from celery import shared_task
 from celery.exceptions import Ignore
 
-from server.analysis.enhancer import Enhancer
-from server.analysis.log_analysis_pipeline import ManualTrainTestPipeline
-from server.analysis.utils.data_filtering import (
-    filter_files,
-    get_file_name_by_orig_file_name,
-    get_prediction_cols,
-)
-from server.analysis.utils.file_level_analysis import (
-    aggregate_file_level,
-    unique_terms_count_by_file,
-)
-from server.analysis.utils.line_level_analysis import (
-    calculate_moving_average_by_columns,
-)
-from server.analysis.utils.log_distance import measure_distances
-from server.analysis.utils.run_level_analysis import (
-    aggregate_run_level,
-    calculate_zscore_sum_anos,
-    files_and_lines_count,
-    unique_terms_count_by_run,
-)
-from server.analysis.utils.umap_analysis import create_umap_df, create_umap_embeddings
-from server.models.settings import Settings
-from server.task_helpers import (
-    create_vectorizer,
-    load_data,
-    store_and_format_result,
+from server.analysis.analysis_runners import (
+    run_anomaly_detection_analysis,
+    run_file_count_analysis,
+    run_log_distance_analysis,
+    run_umap_analysis,
+    run_unique_terms_analysis,
 )
 
 
@@ -50,95 +29,25 @@ def async_run_anomaly_detection(
     mask_type: str,
     vectorizer: str,
 ) -> dict:
-
     start_time = datetime.now(timezone.utc).isoformat()
     meta = {"analysis_type": "Anomaly detection", "start_time": start_time}
-    self.update_state(
-        state="STARTED",
-        meta=meta,
-    )
+    self.update_state(state="STARTED", meta=meta)
 
-    # TODO: Rather than getting boolean values, take str for level
-    if file_level:
-        level = "file"
-    elif directory_level:
-        level = "directory"
-    else:
-        level = "line"
-
-    results = None
-    pipeline = None
     try:
-        settings = Settings.query.filter_by(project_id=project_id).first_or_404()
-        match_filenames = settings.match_filenames
-
-        vectorizer_object = create_vectorizer(vectorizer)
-        pipeline = ManualTrainTestPipeline(
-            model_names=models,
-            item_list_col=item_list_col,
-            vectorizer=vectorizer_object,
-            train_data_path=train_data_path,
-            test_data_path=test_data_path,
-            runs_to_include=runs_to_include,
-            files_to_include=files_to_include,
-            mask_type=mask_type,
+        result = run_anomaly_detection_analysis(
+            project_id,
+            train_data_path,
+            test_data_path,
+            models,
+            item_list_col,
+            runs_to_include,
+            files_to_include,
+            file_level,
+            directory_level,
+            mask_type,
+            vectorizer,
         )
 
-        pipeline.load()
-        pipeline.enhance()
-
-        if match_filenames:
-            if level == "file":
-                pipeline.analyze_file_group_by_filenames()
-            elif level == "line":
-                pipeline.analyze_line_group_by_filenames()
-            else:
-                pipeline.aggregate_to_run_level()
-                pipeline.analyze()
-        else:
-            if level == "file":
-                pipeline.aggregate_to_file_level()
-            elif level == "directory":
-                pipeline.aggregate_to_run_level()
-            pipeline.analyze()
-
-        results = pipeline.results
-        if results is None:
-            raise ValueError("Analysis failed to create results")
-
-        if directory_level or file_level:
-            results = calculate_zscore_sum_anos(
-                results, distance_columns=get_prediction_cols(results)
-            )
-            results = results.drop(item_list_col)
-
-        if level == "line":
-            prediction_columns = [
-                col for col in results.columns if "pred_ano_proba" in col
-            ]
-            df_moving_avg_100 = calculate_moving_average_by_columns(
-                results, 100, prediction_columns
-            )
-            df_moving_avg_10 = calculate_moving_average_by_columns(
-                results, 10, prediction_columns
-            )
-            results = results.with_columns(df_moving_avg_100)
-            results = results.with_columns(df_moving_avg_10)
-
-        analysis_type = f"ano-{level}-level"
-        metadata = {
-            "train_data_path": train_data_path,
-            "test_data_path": test_data_path,
-            "analysis_sub_type": "anomaly-detection",
-            "vectorizer": vectorizer,
-            "item_list_col": item_list_col,
-            "mask_type": mask_type,
-            "models": ";".join(models),
-            "analysis_level": level,
-            "match_filenames": match_filenames if level != "directory" else None,
-        }
-
-        result = store_and_format_result(results, project_id, analysis_type, metadata)
         completed_time = datetime.now(timezone.utc).isoformat()
         meta["completed_time"] = completed_time
 
@@ -159,11 +68,6 @@ def async_run_anomaly_detection(
         )
         raise Ignore()
     finally:
-        if results is not None:
-            del results
-        if pipeline is not None:
-            del pipeline
-
         gc.collect()
 
 
@@ -171,26 +75,11 @@ def async_run_anomaly_detection(
 def async_run_file_counts(self, project_id: int, directory_path: str) -> dict:
     start_time = datetime.now(timezone.utc).isoformat()
     meta = {"analysis_type": "File counts", "start_time": start_time}
-    self.update_state(
-        state="STARTED",
-        meta=meta,
-    )
+    self.update_state(state="STARTED", meta=meta)
 
-    result = None
-    df = None
     try:
-        df = load_data(directory_path)
-        result = files_and_lines_count(df)
+        result = run_file_count_analysis(project_id, directory_path)
 
-        metadata = {
-            "analysis_level": "directory",
-            "directory_path": directory_path,
-            "analysis_sub_type": "file-count",
-        }
-
-        result = store_and_format_result(
-            result, project_id, "directory-level-visualisations", metadata
-        )
         completed_time = datetime.now(timezone.utc).isoformat()
         meta["completed_time"] = completed_time
 
@@ -211,11 +100,6 @@ def async_run_file_counts(self, project_id: int, directory_path: str) -> dict:
         )
         raise Ignore()
     finally:
-        if result is not None:
-            del result
-        if df is not None:
-            del df
-
         gc.collect()
 
 
@@ -223,37 +107,15 @@ def async_run_file_counts(self, project_id: int, directory_path: str) -> dict:
 def async_run_unique_terms(
     self, project_id: int, directory_path: str, item_list_col: str, file_level: bool
 ) -> dict:
-
     start_time = datetime.now(timezone.utc).isoformat()
     meta = {"analysis_type": "Unique terms", "start_time": start_time}
-    self.update_state(
-        state="STARTED",
-        meta=meta,
-    )
+    self.update_state(state="STARTED", meta=meta)
 
-    unique_terms_count = None
-    df = None
     try:
-        df = load_data(directory_path)
-        if not file_level:
-            unique_terms_count = unique_terms_count_by_run(df, item_list_col)
-        else:
-            unique_terms_count = unique_terms_count_by_file(df, item_list_col)
-
-        analysis_type = (
-            "directory-level-visualisations"
-            if not file_level
-            else "file-level-visualisations"
+        result = run_unique_terms_analysis(
+            project_id, directory_path, item_list_col, file_level
         )
-        metadata = {
-            "analysis_sub_type": "unique-terms",
-            "analysis_level": "directory" if not file_level else "file",
-            "directory_path": directory_path,
-        }
 
-        result = store_and_format_result(
-            unique_terms_count, project_id, analysis_type, metadata
-        )
         completed_time = datetime.now(timezone.utc).isoformat()
         meta["completed_time"] = completed_time
 
@@ -274,11 +136,6 @@ def async_run_unique_terms(
         )
         raise Ignore()
     finally:
-        if unique_terms_count is not None:
-            del unique_terms_count
-        if df is not None:
-            del df
-
         gc.collect()
 
 
@@ -294,45 +151,13 @@ def async_create_umap(
 ) -> dict:
 
     start_time = datetime.now(timezone.utc).isoformat()
-    meta = {
-        "analysis_type": "UMAP",
-        "start_time": start_time,
-    }
+    meta = {"analysis_type": "UMAP", "start_time": start_time}
     self.update_state(state="STARTED", meta=meta)
 
-    result = None
-    df = None
     try:
-        df = load_data(directory_path)
-
-        vectorizer_object = create_vectorizer(vectorizer)
-        aggregate_func = aggregate_run_level if not file_level else aggregate_file_level
-        group_col = "run" if not file_level else "seq_id"
-
-        df_agg = (
-            aggregate_func(df, item_list_col, mask_type)
-            .select(item_list_col)
-            .to_series()
-            .to_list()
+        result = run_umap_analysis(
+            project_id, directory_path, item_list_col, file_level, vectorizer, mask_type
         )
-
-        embeddings = create_umap_embeddings(df_agg, vectorizer_object)
-        result = create_umap_df(df, embeddings, group_col=group_col)
-
-        analysis_type = (
-            "directory-level-visualisations"
-            if not file_level
-            else "file-level-visualisations"
-        )
-        metadata = {
-            "analysis_sub_type": "umap",
-            "analysis_level": "directory" if not file_level else "file",
-            "mask_type": mask_type,
-            "vectorizer": vectorizer,
-            "directory_path": directory_path,
-        }
-
-        result = store_and_format_result(result, project_id, analysis_type, metadata)
 
         completed_time = datetime.now(timezone.utc).isoformat()
         meta["completed_time"] = completed_time
@@ -354,11 +179,6 @@ def async_create_umap(
         )
         raise Ignore()
     finally:
-        if result is not None:
-            del result
-        if df is not None:
-            del df
-
         gc.collect()
 
 
@@ -381,45 +201,17 @@ def async_log_distance(
     }
     self.update_state(state="STARTED", meta=meta)
 
-    result = None
-    df = None
     try:
-        settings = Settings.query.filter_by(project_id=project_id).first_or_404()
-        match_filenames = settings.match_filenames
-        vectorizer_object = create_vectorizer(vectorizer)
-
-        enhancer = Enhancer(load_data(directory_path))
-        df = enhancer.enhance_event(item_list_col, mask_type)
-
-        match_flag = file_level and comparison_runs in (None, []) and match_filenames
-        if match_flag:
-            target_file_name = get_file_name_by_orig_file_name(df, target_run)
-            df = filter_files(df, [target_file_name], "file_name")
-
-        run_column = "run" if not file_level else "orig_file_name"
-        result = measure_distances(
-            df,
-            item_list_col,
+        result = run_log_distance_analysis(
+            project_id,
+            directory_path,
             target_run,
-            run_column=run_column,
-            comparison_runs=comparison_runs,
-            vectorizer=vectorizer_object,
+            comparison_runs,
+            item_list_col,
+            file_level,
+            mask_type,
+            vectorizer,
         )
-
-        metadata = {
-            "analysis_level": "directory" if not file_level else "file",
-            "analysis_sub_type": "log-distance",
-            "mask_type": mask_type,
-            "vectorizer": vectorizer,
-            "directory_path": directory_path,
-            "target": target_run,
-            "match_filenames": match_filenames if match_flag else None,
-        }
-        analysis_type = (
-            "distance-file-level" if file_level else "distance-directory-level"
-        )
-
-        result = store_and_format_result(result, project_id, analysis_type, metadata)
 
         completed_time = datetime.now(timezone.utc).isoformat()
         meta["completed_time"] = completed_time
@@ -441,9 +233,4 @@ def async_log_distance(
         )
         raise Ignore()
     finally:
-        if result is not None:
-            del result
-        if df is not None:
-            del df
-
         gc.collect()
