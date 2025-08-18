32a0391651ac46e5de64f2ebc595caba5cdaa6d3
simo <49877847+saimouu@users.noreply.github.com>
2025-08-18 17:40:58 +0300

    Add health check end point

--- server/api/dash_redirects.py
+++ server/api/dash_redirects.py
@@ -6,3 +6,9 @@ dash_redirects_bp = Blueprint("redirects", __name__)
 @dash_redirects_bp.route("/")
 def home():
     return redirect("/dash")
+
+
+# TODO: Move somewere else
+@dash_redirects_bp.route("/health")
+def health():
+    return "OK", 200
